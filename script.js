90f9dae10619b8497d92ebda59afea4f90f954b7
marlonsc15 <marlonsantoscorreia@gmail.com>
2022-08-02 14:07:32 -0300

    adição da função de hora

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// botão que muda de dia para noite
+
 const checkbox = document.getElementById("checkbox");
 
 checkbox.addEventListener("change", () => {
     document.body.classList.toggle("dark");
 });
+
+// audio de mudança
 
 var music = new Audio("transição.mp3");
 
@@ -11,12 +15,19 @@ function troca() {
     music.currentTime = 0.3;
 }
 
+//função de hora do dia
 
+var data = new Date();
+var hora =  data.getHours();
+// var hora = 1;
 
-
- 
-
-
-
-
-
+function ativarCheckbox(el) {
+    el.checked = true;
+}
+    console.log(hora);
+if((hora >= 18 && hora <= 24) || (hora >= 0 && hora < 6)) {
+    document.body.classList.toggle("dark");
+    ativarCheckbox(checkbox);
+}else{
+    
+}
